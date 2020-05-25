e53b3f94d6151c77994bbad8bc8b30b5280f2e0e
Rebecca Mills <beccamills86@gmail.com>
2020-05-25 11:03:11 -0230

    Update install_sbt.sh

--- .startup/install_sbt.sh
+++ .startup/install_sbt.sh
@@ -3,7 +3,7 @@
 set -x
 
 if [ ! -x sbt ] && [ ! -x "$(command -v sbt)" ] ; then
-    if [[ $HOSTNAME == "node"* ]] ; then
+    if [[ $HOSTNAME == "ds201-node1"* ]] ; then
         #rightscale
         echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
         sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
